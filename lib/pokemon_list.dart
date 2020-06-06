100513cc1687c2d7df2742a51aa49a4facf36f6a
MCT <celaltok2@gmail.com>
2020-06-06 16:14:48 +0300

    created pokedexModel and pokemonList class

---
+++ lib/pokemon_list.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+
+class PokemonList extends StatefulWidget {
+  @override
+  _PokemonListState createState() => _PokemonListState();
+}
+
+class _PokemonListState extends State<PokemonList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
